f37942f641701a327cf761349b7bd7bb92baea23
abc <clvlajrrhtlvek@gmail.com>
2025-07-22 21:33:09 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import MainPage from './MainPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/MainPage.jsx
@@ -0,0 +1,7 @@
+function MainPage () {
+    return (
+        <h1 className="text-red-500 text-3xl font-bold underline"> DocQ  메인 페이지</h1>
+    );
+}
+
+export default MainPage;
