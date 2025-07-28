7952c7b533849bc8af8990dcd68142cec5cf08ab
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-07-28 19:09:21 +0900

    style(DQ-48): change position

--- src/components/ClinicSearchResultPage.jsx
+++ src/components/ClinicSearchResultPage.jsx
@@ -4,8 +4,8 @@ import { Pagination } from './Pagenation';
 
 function ClinicData({ name, address, department, open, close, starPoint }) {
     return (
-        <div className="flex flex-col items-center w-screen mx-auto min-h-[calc(90vh-6rem) mb-10]">
-            <div className="shadow-md rounded-lg p-10 px-10 pr-100">
+        <div className="flex flex-col items-center w-screen mx-auto mb-15">
+            <div className="shadow-md rounded-lg p-10 px-10 w-[800px] h-[300px]">
                 <h2 className="font-bold text-5xl">{name} <span className="text-xl">{starPoint}</span></h2>
                 <p className="mt-5 text-2xl"> {open} ~ {close}</p>
                 <p className="mt-5 text-2xl">{address}</p>
@@ -20,18 +20,20 @@ function ClinicSearchResultPage() {
         <>
             <MainHeader />
 
-            {/* 예시 */}
-            <ClinicData
-                name="밝은 성모 안과"
-                address="주소"
-                department="안과"
-                open="09:00"
-                close="16:30"
-                starPoint="별점: 4"
-            />
+            <main className="flex-1 overflow-y-auto pt-[8rem] pb-[5rem]">
+                {/* 예시 */}
+                <ClinicData
+                    name="밝은 성모 안과"
+                    address="주소"
+                    department="안과"
+                    open="09:00"
+                    close="16:30"
+                    starPoint="별점: 4"
+                />
+            </main>
 
             {/* 추후 백엔드 연동 시 num, size 수정*/}
-            <footer className="flex fixed bottom-5 left-0 right-0 ">
+            <footer className="flex fixed bottom-5 left-0 right-0">
                 <Pagination
                     num="1"
                     size="1"
