8a0cfcc9f430c3d0ad6e66ce8c97daf0464d06de
seungminJ <seungmin103@gmail.com>
2025-08-04 10:44:27 +0900

    refactor(DQ-37): resolve merge conflict with App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import MainPage from './components/MainPage'
 import SignUpPage from './components/SignUpPage'
 import SignInPage from './components/SignInPage'
 import ClinicSearchPage from './components/ClinicSearchResultPage'
-<<<<<<< HEAD
 
 import MyPage from './components/MyPage'
 import Board from './components/Board'
-=======
 import ReviewPage from './components/ReviewPage'
 import ClinicPage from './components/ClinicPage'
 import MyPage from './components/MyPage'
->>>>>>> de06cacb3da4dd2c01ab10548914405390b8ddd0
 
 
 function App() {
@@ -30,11 +27,8 @@ function App() {
           <Route path="/search" element={<ClinicSearchPage />} />
           <Route path="/review" element={<ReviewPage />} />
           <Route path='/mypage' element={<MyPage />} />
-<<<<<<< HEAD
           <Route path='/board' element={<Board />} />
-=======
           <Route path="/clinic" element={<ClinicPage />} />
->>>>>>> de06cacb3da4dd2c01ab10548914405390b8ddd0
         </Routes>
       </Router>
     </>
