95af7bd823d554c3ba39d186373058529e5675ba
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-08-08 14:37:35 +0900

    Add NavbarMenu Component

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
 
+function NavbarMenu({ url, menuName, isSubMenu = false }) {
+    return (
+        <Link to={url}>
+            <span className="font-bold text-2xl px-10 py-10">{menuName}</span>
+        </Link>
+    );
+}
+
 function NavigationBar() {
     return (
-        <header className="fixed top-0 left-0 right-0 bg-white shadow px-6 py-5">
-            <nav className="flex flex-row justify-center items-center">
+        <header className="fixed top-0 left-0 right-0 bg-white shadow px-25 py-5">
+            <nav className="flex flex-row justify-between items-center w-full">
                 {/* 로고 */}
                 <Link to='/'>
                     <img
@@ -14,38 +22,50 @@ function NavigationBar() {
                     />
                 </Link>
 
-                {/* home */}
-                <Link to='/'>
-                    <span className="font-bold text-2xl mr-10">메인</span>
-                </Link>
+                <div className="flex flex-1 flex-row">
+                    <ul className="flex flex-row">
+                        <li>
+                            <NavbarMenu
+                                url="/"
+                                menuName="메인"
+                            />
+                        </li>
+                        <li>
+                            <NavbarMenu
+                                url="/board"
+                                menuName="게시판"
+                            />
+                        </li>
+                        <li>
+                            <NavbarMenu
+                                url="/mypage"
+                                menuName="마이 페이지"
+                                isSubMenu="true"
+                            />
+                        </li>
+                        <li>
+                            <NavbarMenu
+                                url="/"
+                                menuName="병원"
+                                isSubMenu="true"
+                            />
+                        </li>
+                    </ul>
+                </div>
 
-                {/* 게시판 */}
-                <Link to='/board'>
-                    <span className="font-bold text-2xl mr-10">게시판</span>
-                </Link>
-
-                {/* 마이페이지 */}
-                <Link to='/mypage'>
-                    <span className="font-bold text-2xl mr-10">마이 페이지</span>
-                </Link>
-
-                {/* 병원 */}
-                <p>
-                    <span className="font-bold text-2xl mr-10">병원</span>
-                </p>
-
-
-                <form role="search" className="flex flex-1 max-w-md">
-                    <input
-                        type="search"
-                        placeholder="검색..."
-                        aria-label="Search"
-                        className="w-full px-3 py-2 border border-gray-300 rounded"
-                    />
-                </form>
-                <button className="text-white bg-black px-4 py-2 rounded">
-                    검색
-                </button>
+                <div className="flex flex-row">
+                    <form role="search" className="flex max-w-md px-5">
+                        <input
+                            type="search"
+                            placeholder="검색..."
+                            aria-label="Search"
+                            className="w-full px-3 py-2 border border-gray-300 rounded"
+                        />
+                    </form>
+                    <button className="text-white bg-black px-4 py-2 rounded">
+                        검색
+                    </button>
+                </div>
             </nav>
         </header>
     );
