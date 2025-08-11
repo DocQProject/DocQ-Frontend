4ce061d8fe7d1eba2c8ef3e7bef30d06f3419b19
jiwon <clvlajrrhtlvek@gmail.com>
2025-08-11 20:23:38 +0900

    refactor(DQ-43): Apply getDepartments method

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,18 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react"
 
-function DepartmentIcon({ imageUrl, name }) {
-    return (
-        <div className="text-center bg-gray-200 px-10 py-7 rounded-lg shadow">
-            <img
-                src={imageUrl}
-            />
-            <p className="font-bold">{name}</p>
-        </div>
-    );
-}
-
-function MainPage() {
+function getDepartments() {
     const [departments, setDepartments] = useState([]);
     const token = "eyJhbGciOiJIUzUxMiJ9.eyJpZCI6NSwibG9naW5JZCI6IuyVhOydtOuUlCIsIm5hbWUiOiLshJzsp4Dsm5AiLCJyb2xlIjoiUk9MRV9BRE1JTiIsImV4cCI6MTc1NDkzMzIxMSwiaWF0IjoxNzU0ODkwMDExfQ.A9C7_-WFhkMiWhoXSldtCnzTZr2Hd53rypE_2gpXroW0EGhau-QWIMCd7jqJaeAphp-GOPqnkl-lIyjoIhQGfg"
 
@@ -30,6 +19,21 @@ function MainPage() {
             .catch(err => console.log(err));
     }, []);
 
+    return departments;
+}
+
+function DepartmentIcon({ imageUrl, name }) {
+    return (
+        <div className="text-center bg-gray-200 px-10 py-7 rounded-lg shadow">
+            <img
+                src={imageUrl}
+            />
+            <p className="font-bold">{name}</p>
+        </div>
+    );
+}
+
+function MainPage() {
     return (
         <>
             <main>
@@ -37,7 +41,7 @@ function MainPage() {
                 <div className="w-screen mx-auto px-100 flex-1 pt-[10rem] pb-[5rem]">
                     <p className="font-bold text-xl mb-10">진료과로 병원 찾기</p>
                     <div className="shadow-md rounded-lg p-10 flex gap-10 justify-center w-full overflow-x-auto scrollbar-hide">
-                        {departments.map((department, index) =>
+                        {getDepartments().map((department, index) =>
                             <DepartmentIcon
                                 key={index}
                                 name={department.name}
