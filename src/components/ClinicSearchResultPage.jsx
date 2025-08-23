c1e8c7c8e858f3230cbd52011592477b1e3ecd55
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-08-23 19:15:10 +0900

    refactor(DQ-57): Add error handling for empty search results

--- src/components/ClinicSearchResultPage.jsx
+++ src/components/ClinicSearchResultPage.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Pagination } from './Pagenation';
-import { useSearchParams  } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { fetchSearchClinicByQuery } from '../api';
 
-function ClinicData({ name, address, department, open, close, starPoint }) {
+function ClinicData({ id, name, address, department, open, close, starPoint }) {
+    const navigate = useNavigate();
     return (
-         <div className="flex justify-center w-full px-4 mb-10">
-            <div className="w-full md:w-[750px] bg-white rounded-2xl shadow-md p-8 border border-gray-200 hover:shadow-lg transition-shadow duration-300">
-                
+        <div className="flex justify-center w-full px-4 mb-10">
+            <div
+                className="w-full md:w-[750px] bg-white rounded-2xl shadow-md p-8 border border-gray-200 hover:shadow-lg transition-shadow duration-300"
+                onClick={() => navigate(`/clinic/${id}`)}
+            >
+
                 {/* 병원명 + 별점 */}
                 <div className="flex items-center justify-between border-b pb-3 mb-4">
                     <h2 className="font-bold text-3xl text-gray-800">{name}</h2>
@@ -36,28 +40,31 @@ function ClinicSearchResultPage() {
     useEffect(() => {
         const keyword = searchParams.get("q");
         fetchSearchClinicByQuery(keyword, currentPage)
-        .then((res) => {
-            console.log(res.data);
-            setTotalPages(res.data.page.totalPages)
-            setSearchResult(res.data.content)
-        })
+            .then((res) => {
+                console.log(res.data);
+                setTotalPages(res.data.page.totalPages)
+                setSearchResult(res.data.content)
+            })
     }, [searchParams, currentPage])
 
     return (
         <>
             <main className="flex-1 overflow-y-auto pt-[10rem] pb-[5rem]">
                 {
-                    searchResult.map((result, index) =>
-                        <ClinicData
-                            key={index}
-                            name={result.name}
-                            address={result.address}
-                            department={result.department}
-                            open={result.openTime}
-                            close={result.closeTime}
-                            starPoint={result.starPoint}
-                        />
-                    )
+                    searchResult.length === 0 ?
+                        <p className='flex justify-center items-center text-2xl font-bold'>검색 결과가 존재하지 않습니다.</p>
+                        : searchResult.map((result, index) =>
+                            <ClinicData
+                                key={index}
+                                id={result.clinicId}
+                                name={result.name}
+                                address={result.address}
+                                department={result.department}
+                                open={result.openTime}
+                                close={result.closeTime}
+                                starPoint={result.starPoint}
+                            />
+                        )
                 }
             </main>
 
